5b93fda9ca13371102b918111e630c971848cdd1
tarunsingh21126-jpg <tarunsingh21126@gmail.com>
2026-03-29 23:51:46 +0530

    fix-2

--- BACKEND/core/settings.py
+++ BACKEND/core/settings.py
@@ -25,7 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-8rw&g66zm&b_m$$g7z89d_yua2f#sli+!+z@!638bzc%i-646#'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ["*"]
 
