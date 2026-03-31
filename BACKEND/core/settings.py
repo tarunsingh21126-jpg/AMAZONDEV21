d722a2ab6a427ffbbae2896bdc28eeca48d93bde
tarunsingh21126-jpg <tarunsingh21126@gmail.com>
2026-04-01 02:41:48 +0530

    vfic

--- BACKEND/core/settings.py
+++ BACKEND/core/settings.py
@@ -79,21 +79,21 @@ WSGI_APPLICATION = 'core.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/6.0/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': os.getenv('DB_NAME'),
-        'USER': os.getenv('DB_USER'),
-        'PASSWORD': os.getenv('DB_PASSWORD'),
-        'HOST': os.getenv('DB_HOST'),
-        'PORT': os.getenv('DB_PORT'),
-    }
- }
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql',
+#         'NAME': os.getenv('DB_NAME'),
+#         'USER': os.getenv('DB_USER'),
+#         'PASSWORD': os.getenv('DB_PASSWORD'),
+#         'HOST': os.getenv('DB_HOST'),
+#         'PORT': os.getenv('DB_PORT'),
+#     }
+#  }
 # import dj_database_url
 
-# DATABASES = {
-#      'default': dj_database_url.config(default='postgresql://amazondb21_user:bjhfCsg3PvzGmqDnOWsmyMeqEntATwAc@dpg-d743lpp5pdvs7380varg-a.virginia-postgres.render.com/amazondb21')
-# }
+DATABASES = {
+      'default': dj_database_url.config(default='postgresql://amazondb21_user:bjhfCsg3PvzGmqDnOWsmyMeqEntATwAc@dpg-d743lpp5pdvs7380varg-a.virginia-postgres.render.com/amazondb21')
+}
 
 # Password validation
 # https://docs.djangoproject.com/en/6.0/ref/settings/#auth-password-validators
