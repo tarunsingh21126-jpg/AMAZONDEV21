84bfadc617fdebe5fe1b7eefe21667d454db9582
tarunsingh21126-jpg <tarunsingh21126@gmail.com>
2026-03-31 23:05:56 +0530

    fix path import

--- BACKEND/core/urls.py
+++ BACKEND/core/urls.py
@@ -26,7 +26,10 @@ Including another URLconf
 
 # if settings.DEBUG:
 #     urlpatterns += static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
-
+from django.contrib import admin
+from django.urls import path,include
+from django.conf import settings
+from django.conf.urls.static import static
 from django.http import HttpResponse
 from django.contrib.auth import get_user_model
 
